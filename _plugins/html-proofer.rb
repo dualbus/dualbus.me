fb42d86a49573f4bfc4e56de4c36563669995150
Eduardo A. Bustamante López <dualbus@gmail.com>
2019-06-01 15:49:50 -0700

    Add favicon
    <https://www.favicon.cc/?action=icon&file_id=19295>
    
    Created: 11 years ago
    From: rgrwkmn
    License: Creative Commons, no attribution

--- _plugins/html-proofer.rb
+++ _plugins/html-proofer.rb
@@ -1,6 +1,20 @@
 # <https://alexwlchan.net/2019/02/checking-jekyll-sites-with-htmlproofer/>
 require 'html-proofer'
 
+# <https://github.com/gjtorikian/html-proofer>
+options = {
+  check_external_hash: true,
+  check_favicon: true,
+  check_opengraph: true,
+  check_html: true,
+  validation: {
+    # report_invalid_tags: true, # FIXME:
+    report_missing_names: true,
+  },
+  check_img_http: true,
+  # enforce_https: true, # FIXME:
+}
+
 Jekyll::Hooks.register :site, :post_write do |site|
-  HTMLProofer.check_directory(site.config['destination']).run
+  HTMLProofer.check_directory(site.config['destination'], options).run
 end
